56a3da02c85ce18489fbab38519383365c3ed85c
Yegor Knyazev <egorknyazev10@gmail.com>
2025-06-19 11:09:39 +0400

    install sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@nuxtjs/google-fonts']
+  modules: [
+    'vuetify-nuxt-module',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/sitemap'
+  ]
 })
