493536f2a9671801f7313cc4ba5e704b4f0e2415
Yegor Knyazev <egorknyazev10@gmail.com>
2025-06-19 11:36:35 +0400

    add settings and seo

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+useSeoMeta({
+  ogTitle: 'Клуб Кубит',
+  ogDescription: 'Клуб программирования и дизайна Кубит. Находится в городе Глазов',
+  ogImage: 'https://qbit-club.com/og-image.jpg',
+  ogUrl: 'https://qbit-club.com/',
+  ogLocale: 'ru-RU',
+  twitterTitle: 'Клуб Кубит',
+  twitterDescription: 'Клуб программирования и дизайна Кубит. Находится в городе Глазов',
+  twitterImage: 'https://qbit-club.com/og-image.jpg',
+  twitterCard: 'summary',
+  applicationName: 'Клуб Кубит',
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { ru } from 'vuetify/locale'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
@@ -7,5 +10,100 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     '@nuxtjs/sitemap',
     '@nuxtjs/robots'
-  ]
+  ],
+  
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ru'
+      },
+      title: 'Клуб Кубит',
+      meta: [{
+        name: 'description', content: 'Клуб программирования и дизайна Кубит. Находится в городе Глазов'
+      }],
+      link: [
+        { rel: 'icon', href: '/logo.svg' },
+      ]
+    }
+  },
+
+  site: { 
+    url: 'https://qbit-club.com', 
+    name: 'Клуб Кубит' 
+  }, 
+
+  routeRules: {
+    '/': { prerender: true },
+  },
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ['vue-router']
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  },
+
+  robots: {
+    groups: [
+      { userAgent: '*', allow: '/' },
+    ]
+  },
+
+  sitemap: {
+    urls: [
+      { loc: '/', lastmod: new Date().toISOString(), priority: 1, changefreq: 'weekly' },
+    ]
+  },
+
+
+  vuetify: {
+    vuetifyOptions: {
+      locale: {
+        locale: 'ru',
+        fallback: 'en',
+        messages: { ru }
+      },
+      theme: {
+        defaultTheme: 'light',
+        themes: {
+          light: {
+            colors: {
+              primary: '#334064'
+            },
+          }
+        }
+      },
+      defaults: {
+        VBtn: {
+          class: 'text-body-2',
+          style: 'border-radius: 8px;'
+        },
+        global: {
+          ripple: false,
+          elevation: 0,
+        },
+      },
+      icons: {
+        defaultSet: 'mdi-svg',
+      }
+    }
+  },
+
+  googleFonts: {
+    families: {
+      Montserrat: [400, 500, 700]
+    },
+    download: true,
+    preload: true,
+  },
 })
