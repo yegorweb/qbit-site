300ee03b3192bc9a3a15e10a0c80b45afd111081
Yegor Knyazev <egorknyazev10@gmail.com>
2025-06-19 11:08:51 +0400

    install google-fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module']
+  modules: ['vuetify-nuxt-module', '@nuxtjs/google-fonts']
 })
