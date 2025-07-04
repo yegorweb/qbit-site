21e8df7db9c39da1dc1e12aab1893716b51e2616
Yegor Knyazev <egorknyazev10@gmail.com>
2025-07-04 22:21:40 +0400

    add title component

---
+++ components/BlockTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="font-weight-bold" style="font-size: clamp(1.125rem, 0.8864rem + 1.0909vw, 1.5rem); margin-bottom: 10px; user-select: none;"><slot /></div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
