7986735f707cd9b8b2f0636b6d55e411e750d72a
Yegor Knyazev <egorknyazev10@gmail.com>
2025-07-08 16:25:54 +0400

    create agreement page (base for Роман)

---
+++ pages/agreement.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <BlockTitle>Согласие об обработке персональных данных</BlockTitle>
+
+    <p>
+      
+    </p>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
