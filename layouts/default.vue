2465b44cf64d7de2ee115ce6d6741096d8eebc65
Yegor Knyazev <egorknyazev10@gmail.com>
2025-07-08 16:22:56 +0400

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <v-app>
+  <v-app style="min-height: 100vh;">
     <div style="background: #FFFFFF; position: fixed; top: 0; left: 0; right: 0; height: 60px; z-index: 999;">
       <v-container class="d-flex align-center justify-space-between user-select-none"
         style="height: 60px; position: relative;">
@@ -17,6 +17,19 @@
     <div style="height: 60px;"></div>
 
     <NuxtPage />
+
+    <v-spacer></v-spacer>
+
+    <footer>
+      <v-container style="display: flex;">
+        <div style="margin: -6px">
+          <div 
+            @click="navigateTo('/agreement')" 
+            style="padding: 6px; cursor: pointer; color: #8E8E8E;"
+          >Согласие об обработке персональных данных</div>
+        </div>
+      </v-container>
+    </footer>
   </v-app>
 </template>
 
