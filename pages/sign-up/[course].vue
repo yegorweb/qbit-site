63695a6b3a97a69ce87aa7bf00f03c12dca255ce
qbclub <17926057+qbclub@users.noreply.github.com>
2025-08-05 17:24:50 +0400

    content json

--- pages/sign-up/[course].vue
+++ pages/sign-up/[course].vue
@@ -4,25 +4,15 @@ import { useField, useForm } from 'vee-validate'
 
 let content = useContent()
 
-const courses = ['web-dev', 'scratch', 'java', 'drawing'] as const
-type Course = typeof courses[number]
-let course = useRoute().params.course as Course
+let curentCourse = ref<CourseContent>()
+let courseParam = useRoute().params.course
+let course = Array.isArray(courseParam) ? courseParam[0] : courseParam
 
-const courseNames = new Map<Course, string>([
-  ['web-dev', 'Веб-разработка'], 
-  ['drawing', 'Цифровая живопись'], 
-  ['scratch', 'Scratch'],
-  ['java', 'Java'],
-])
-const titleCourseNames = new Map<Course, string>([
-  ['web-dev', 'веб-разработке'], 
-  ['drawing', 'цифровой живописи'], 
-  ['scratch', 'Scratch'],
-  ['java', 'Java'],
-])
-let contentKeys = new Map<Course, keyof Content>([['web-dev', 'webdevinfo'], ['drawing', 'drawinginfo'], ['scratch', 'scratchinfo'], ['java', 'javainfo']])
+if (import.meta.client) {
+  curentCourse.value = content.value[course as keyof Content]
+}
 
-if (!courseNames.get(course)) showError({ statusCode: 404, message: 'Курс не найден' })
+// if (!courseNames.get(course)) showError({ statusCode: 404, message: 'Курс не найден' })
 
 let { handleSubmit, handleReset } = useForm({
   initialValues: {
@@ -73,7 +63,7 @@ let submitForm = handleSubmit(async values => {
   showSuccessPopup.value = false
 
   let body = {
-    'Курс': courseNames.get(course as Course),
+    'Курс': (curentCourse.value as CourseContent).name,
     'Имя': values.fullname,
     'Возраст': `${values.age} лет`,
     'Телефон': `<a href="tel:${values.phone.replaceAll(/\D/g, '')}">${values.phone}</a>`
@@ -93,7 +83,7 @@ let submitForm = handleSubmit(async values => {
     })
     if (response.status === 200) {
       showSuccessPopup.value = true
-      reset()    
+      reset()
     } else {
       showErrorPopup.value = true
     }
@@ -118,94 +108,63 @@ function reset() {
   <v-container class="page-container">
     <BackButton />
 
-    <BlockTitle>Курс по {{ titleCourseNames.get(course as Course) }}</BlockTitle>
+
+    <!-- <BlockTitle>Курс по {{ titleCourseNames.get(course as Course) }}</BlockTitle> -->
 
     <v-row>
       <v-col cols="12" md="auto">
         <div class="card card-info" style="padding-top: 20px">
           <v-form @submit.prevent="submit" class="form">
-            <v-text-field
-              v-model="fullname.value.value"
-              :error-messages="submited?fullname.errorMessage.value:undefined"
-              label="Имя Фамилия"
-              placeholder="Иван Иванов"
-              variant="outlined"
-              density="compact"
-              autocomplete="name"
-            />
+            <v-text-field v-model="fullname.value.value"
+              :error-messages="submited ? fullname.errorMessage.value : undefined" label="Имя Фамилия"
+              placeholder="Иван Иванов" variant="outlined" density="compact" autocomplete="name" />
 
-            <v-number-input
-              v-model="age.value.value"
-              :error-messages="submited?age.errorMessage.value:undefined"
-              :class="{'mt-3':submited&&!!fullname.errorMessage.value}"
-              control-variant="hidden"
-              label="Возраст, лет"
-              placeholder="14"
-              variant="outlined"
-              density="compact"
-              pattern="[0-9]*" 
-              inputmode="numeric"
-              autocomplete="off"
-            />
+            <v-number-input v-model="age.value.value" :error-messages="submited ? age.errorMessage.value : undefined"
+              :class="{ 'mt-3': submited && !!fullname.errorMessage.value }" control-variant="hidden"
+              label="Возраст, лет" placeholder="14" variant="outlined" density="compact" pattern="[0-9]*"
+              inputmode="numeric" autocomplete="off" />
 
-            <v-text-field
-              v-model="phone.value.value"
-              :error-messages="submited?phone.errorMessage.value:undefined"
-              :class="{'mt-3':submited&&!!age.errorMessage.value}"
-              label="Телефон"
-              variant="outlined"
-              density="compact"
-              placeholder="+7 (999) 999-99-99"
-              id="phone-input"
-              autocomplete="tel"
-              type="tel"
-            />
+            <v-text-field v-model="phone.value.value" :error-messages="submited ? phone.errorMessage.value : undefined"
+              :class="{ 'mt-3': submited && !!age.errorMessage.value }" label="Телефон" variant="outlined"
+              density="compact" placeholder="+7 (999) 999-99-99" id="phone-input" autocomplete="tel" type="tel" />
 
-            <v-checkbox 
-              v-model="consent.value.value"
-              :error-messages="submited?consent.errorMessage.value:undefined"
-              :class="{'mt-3':submited&&!!phone.errorMessage.value}"
-              color="primary"
-              density="comfortable"
-            >
+            <v-checkbox v-model="consent.value.value"
+              :error-messages="submited ? consent.errorMessage.value : undefined"
+              :class="{ 'mt-3': submited && !!phone.errorMessage.value }" color="primary" density="comfortable">
               <template v-slot:label>
-                <div>Даю <a
-                  href="/agreement"
-                  style="text-decoration: none;"
-                  target="_blank"
-                  @click.stop
-                >согласие на обработку персональных данных</a></div>
+                <div>Даю <a href="/agreement" style="text-decoration: none;" target="_blank" @click.stop>согласие на
+                    обработку персональных данных</a></div>
               </template>
             </v-checkbox>
 
-            <v-btn 
-              type="submit"
-              :loading="loading"
-              :class="{'w-100':true,'mt-3':submited&&!!consent.errorMessage.value,'bg-primary':true}" 
-            >Записаться</v-btn>
+            <v-btn type="submit" :loading="loading"
+              :class="{ 'w-100': true, 'mt-3': submited && !!consent.errorMessage.value, 'bg-primary': true }">Записаться</v-btn>
           </v-form>
         </div>
       </v-col>
 
       <v-col cols="12" md="auto">
         <ClientOnly>
-          <div v-html="content[contentKeys.get(course)!]" />
+          <img :src="curentCourse?.image" alt="">
+          {{ curentCourse }}
         </ClientOnly>
       </v-col>
+
     </v-row>
   </v-container>
 
   <v-dialog v-model="showSuccessPopup" max-width="450">
     <template v-slot:default="{ isActive }">
       <div class="d-flex flex-column justify-center align-center" style="margin-top: -30px;">
-        <v-icon size="70" color="white" class="bg-green" :icon="mdiCheck" style="margin-bottom: -30px; z-index: 999; padding: 10px; border-radius: 999px;"></v-icon>
-        
+        <v-icon size="70" color="white" class="bg-green" :icon="mdiCheck"
+          style="margin-bottom: -30px; z-index: 999; padding: 10px; border-radius: 999px;"></v-icon>
+
         <v-card class="status-card rounded-lg" style="padding: 10px 24px;">
           <div class="mt-6 text-center">
-            <span style="font-weight: 500; font-size: 24px;">Вы записаны</span><br/>
+            <span style="font-weight: 500; font-size: 24px;">Вы записаны</span><br />
             <span style="font-weight: 400; font-size: 15px;">Скоро мы вам позвоним</span>
           </div>
-  
+
           <v-btn @click="isActive.value = false" class="bg-grey-lighten-3 mt-5">Закрыть</v-btn>
         </v-card>
       </div>
@@ -215,14 +174,15 @@ function reset() {
   <v-dialog v-model="showErrorPopup">
     <template v-slot:default="{ isActive }">
       <div class="d-flex flex-column justify-center align-center" style="margin-top: -30px;">
-        <v-icon size="70" color="white" class="bg-red" :icon="mdiClose" style="margin-bottom: -30px; z-index: 999; padding: 10px; border-radius: 999px;"></v-icon>
-        
+        <v-icon size="70" color="white" class="bg-red" :icon="mdiClose"
+          style="margin-bottom: -30px; z-index: 999; padding: 10px; border-radius: 999px;"></v-icon>
+
         <v-card class="status-card" style="padding: 14px; border-radius: 16px;">
           <div class="mt-5 text-center">
-            <span style="font-weight: 500; font-size: 24px;">Ошибка</span><br/>
+            <span style="font-weight: 500; font-size: 24px;">Ошибка</span><br />
             <span style="font-weight: 400; font-size: 15px;">Не удалось отправить форму</span>
           </div>
-  
+
           <div class="d-flex mt-5 flex-wrap flex-row align-center justify-center" style="gap: 5px;">
             <v-btn @click="submit()" :prepend-icon="mdiReload" class="bg-grey-lighten-3">Повторить</v-btn>
             <v-btn @click="isActive.value = false" class="bg-grey-lighten-3">Закрыть</v-btn>
@@ -237,7 +197,7 @@ function reset() {
 .card {
   max-width: 365px;
 
-  @media screen and (width >= 960px) {
+  @media screen and (width >=960px) {
     & {
       min-width: 365px;
     }
@@ -251,7 +211,7 @@ function reset() {
   align-items: center;
 }
 
-.form > * {
+.form>* {
   transition: all .15s;
 }
 
