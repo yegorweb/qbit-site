9dcac7a7ee3c2150bc10c45c2a5b7feeeb63c677
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-05 16:04:40 +0400

    review types

--- types/content.d.ts
+++ types/content.d.ts
@@ -1,10 +1,22 @@
 declare global {
+  interface CourseContent {
+    name: string // html
+    price: number // руб/занятие
+    min_age: number // years
+    description: string // html
+    teacher: {
+      name: string // html
+      avatar: string // url
+    }
+    image: string // url
+    duration: number // минут в занятии
+    frequency: number // занятий в неделю
+  }
 	interface Content {
-    qbit: string
-	  webdevinfo: string
-    drawinginfo: string
-    scratchinfo: string
-    javainfo: string
+	  webdevinfo: CourseContent
+    drawinginfo: CourseContent
+    scratchinfo: CourseContent
+    javainfo: CourseContent
   }
 }
 
